d4fc208b79881e5e96bb3904d57199de6ea8aec5
Joe Victor Santos <joe.santos@aliare.co>
2022-03-22 13:53:33 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+txtUrl = "";
+async function  copiar(){
+    var content = document.getElementById('link');
+    content.disabled = false;
+    await content.select();
+    content.disabled = true;
+    document.execCommand('copy');
+    document.getElementById('txtStatus').innerHTML="✔️Copiado!"
+}
+function gerarLink(){
+    txtUrl = "https://api.whatsapp.com/send?phone=55";
+    numero=document.getElementById("telefone").value;
+    numero = numero.replaceAll(' ','').replaceAll('(','').replaceAll(')','').replaceAll('-','')
+    if(numero!=''){
+        txtUrl+=numero;
+        mensagem = document.getElementById("mensagem").value;
+        if(mensagem!=''){
+            mensagem = mensagem.replaceAll(' ',"%20");
+            txtUrl +="&text="+ mensagem; 
+        }
+        document.getElementById("link").value =txtUrl;
+        document.getElementById("divLink").style.display="block";
+    }
+    else{
+        alert ("Digite o número do whatsapp!")
+    }
+}
+function verificarNumero(str){
+    return str.replaceAll(/[^0-9]/g,'')
+  }
+
+function telefoneConfig(){
+    telefone = document.getElementById('telefone');
+    numero = verificarNumero(telefone.value);
+    saida="";
+    for(i =0 ; i <numero.length; i++){
+        switch(i){
+            case 0: saida = '('+numero[i]; break;
+            case 1: saida += numero[i]+") "; break;
+            case 6: saida += numero[i]+'-'; break;
+            default: saida += numero[i];
+        }
+    }
+    document.getElementById('telefone').value =saida;
+}
